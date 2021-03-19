c8432c8daf4011d56d9e50cefa9fb2be911e27ea
growinghsb <seungbeombm@gmail.com>
2021-03-19 17:04:52 +0900

    Add project

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class StringCalculator {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
