b2eb64e878fdb7c38f7ed85764ca51e4c9ee6694
growinghsb <seungbeombm@gmail.com>
2021-03-20 17:09:47 +0900

    Add input fomula

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,11 +2,27 @@ package calculator;
 
 import java.util.Scanner;
 
+/**
+ * 구현 기능
+ * 1. 문자열을 입력 받는다.
+ * 2. 해당 문자열이 비었는지, 검사한다.
+ * 3. 커스텀 문자가 포함 되었는지 확인한다.
+ *  3-1. 커스텀 문자 = //Custom<<의 양식을 따른다
+ * 4. 문자열을 자른다.
+ * 5. 계산한다.
+ */
+
 public class StringCalculator {
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("시작");
+
     }
+
+    public static String inputFormula(){
+        return scanner.nextLine();
+    }
+
+
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+
+
 
 }
