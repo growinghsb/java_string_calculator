fead48b4d53b0af7e327f75ed7fd2f9f06505fa6
growinghsb <seungbeombm@gmail.com>
2021-03-20 16:11:29 +0900

    Add setting

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,6 +7,6 @@ public class StringCalculator {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        System.out.println("시작");
     }
 }
